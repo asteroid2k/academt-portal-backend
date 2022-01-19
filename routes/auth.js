54ea7f259bc687dc64e5227ed0a38a8a5d677fe7
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-01-19 15:22:50 +0000

    admin authentication and middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
-const { login, register } = require("../controllers/authController");
+const {
+  login,
+  register,
+  loginAdmin,
+} = require("../controllers/authController");
 const { validateData } = require("../middleware/validation");
 const { loginValidator, registerValidator } = require("../util/validators");
 
 const authRouter = Router();
 
 authRouter.post("/signin", validateData(loginValidator), login);
+authRouter.post("/admin/signin", validateData(loginValidator), loginAdmin);
 authRouter.post("/signup", validateData(registerValidator), register);
 module.exports = authRouter;
